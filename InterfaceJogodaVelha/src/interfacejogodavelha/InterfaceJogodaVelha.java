82dee6d4fc9da7acb9f818ef43334dc4d421bb82
marcelofaria <marceloasf952@gmail.com>
2017-05-29 22:47:33 -0300

    Interface da tela do Jogo com algumas funcionalidades implementadas

---
+++ InterfaceJogodaVelha/src/interfacejogodavelha/InterfaceJogodaVelha.java
@@ -0,0 +1,23 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacejogodavelha;
+/**
+ *
+ * @author Talita-csl
+ */
+public class InterfaceJogodaVelha {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Jogo2 frame = new Jogo2();
+        frame.setVisible(true);
+         
+    }
+    
+}
---
+++ InterfaceJogodaVelha/src/interfacejogodavelha/Variaveis.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacejogodavelha;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class Variaveis {
+    String bolinha = "              O";
+    String xis     = "              X";
+    
+    int x;
+    int o;
+}
